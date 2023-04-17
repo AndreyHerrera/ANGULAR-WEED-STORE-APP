df021184f697b8c83f21d1610604a386ff1afcea
Andrey Herrera <andreyherrerac@icloud.com>
2023-04-17 14:43:51 -0500

    Login - Register HTML | Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { enviroment } from '../environments/environment';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : enviroment.pathLogin, component : LoginPageComponent},
+  {path : enviroment.pathRegister, component : RegisterPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+const url = "http://localhost:8081/";
+const path = 'V1/'
+
+export const enviroment = {
+  pathLogin: path + 'login',
+  pathRegister: path + 'register'
+};
+
+export const api = {
+  apiLogin: url + 'login',
+  apiRegister: url + 'register'
+};
