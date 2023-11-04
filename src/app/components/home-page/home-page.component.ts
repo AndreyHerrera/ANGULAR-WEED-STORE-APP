e1d6c042e52e3368eb979718a9a249b240dbddce
AndreyHerrera <andreyherrerac@icloud.com>
2023-11-04 12:11:47 -0500

    Employee component and related files have been removed. HomePageComponent now shows a coming soon message.

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { enviroment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-  public enviromentLogin : string;
-  public enviromentRegister : string;
+  public enviromentLogin: string;
+  public enviromentRegister: string;
+  acceptButton: {} = {};
 
-  constructor(){
-    localStorage.setItem("AuthToken" , "false");
-    this.enviromentLogin = "/" + enviroment.pathLogin;
-    this.enviromentRegister = "/" + enviroment.pathRegister;
+  constructor() {
+    Swal.fire({
+      title: 'You must be 21 years old to enter this site.',
+      icon: 'question',
+      iconHtml: '?',
+      confirmButtonText: 'Yes, I am over 21',
+      cancelButtonText: 'No, I am under 21',
+      showCancelButton: false,
+      showCloseButton: true,
+    });
+
+    localStorage.setItem('AuthToken', 'false');
+    this.enviromentLogin = '/' + enviroment.pathLogin;
+    this.enviromentRegister = '/' + enviroment.pathRegister;
   }
 }
