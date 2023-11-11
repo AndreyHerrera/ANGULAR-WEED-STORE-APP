a813d3fdf0e0913cc391184b6ac00616c5bc2c52
wherre3 <andreyherrerac@icloud.com>
2023-11-11 01:14:56 -0500

    Update code related to age verification and header

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { enviroment } from 'src/environments/environment';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home-page',
@@ -13,16 +12,6 @@ export class HomePageComponent {
   acceptButton: {} = {};
 
   constructor() {
-    Swal.fire({
-      title: 'You must be 21 years old to enter this site.',
-      icon: 'question',
-      iconHtml: '?',
-      confirmButtonText: 'Yes, I am over 21',
-      cancelButtonText: 'No, I am under 21',
-      showCancelButton: false,
-      showCloseButton: true,
-    });
-
     localStorage.setItem('AuthToken', 'false');
     this.enviromentLogin = '/' + enviroment.pathLogin;
     this.enviromentRegister = '/' + enviroment.pathRegister;
