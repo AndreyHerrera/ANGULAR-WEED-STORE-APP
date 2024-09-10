bf7a8b68084faa7c3bbbef80f10ad7f018d8fb7a
wolfangherrera <andreyherrerac@icloud.com>
2024-09-09 22:52:32 -0500

    "Deleted multiple Angular components, including home page, products, footer, and header, along with their HTML, SCSS, and spec files."

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
-const url = "http://127.0.0.1:3000/";
-const path = 'V1/'
+const url = 'http://127.0.0.1:3000/';
+const path = 'V1/';
 
 export const enviroment = {
   pathHome: '',
-  pathLogin: path + 'login',
+  pathLogin: path + '',
   pathRegister: path + 'register',
-  pathEmployee: path + 'employee'
+  pathEmployee: path + 'employee',
 };
 
 export const api = {
   apiLogin: url + path + 'login',
   apiRegister: url + path + 'register',
-  apiAuthToken: url + path + 'authToken'
+  apiAuthToken: url + path + 'authToken',
 };
